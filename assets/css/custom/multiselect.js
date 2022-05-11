5515501d1a30d10768b5e872a1b9fd8e386192ce
iamdhileep <104512274+iamdhileep@users.noreply.github.com>
2022-05-12 00:24:39 +0530

    Add files via upload

---
+++ assets/css/custom/multiselect.js
@@ -0,0 +1,19 @@
+function previewFile() {
+    var preview = document.querySelector('img');
+    var file    = document.querySelector('input[type=file]').files[0];
+    var reader  = new FileReader();
+
+    reader.addEventListener("load", function () {
+        preview.src = reader.result;
+    }, false);
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+}
+
+
+$(document).ready(function(){
+    var input = document.querySelector('input[name=tags]');
+    new Tagify(input)
+});
